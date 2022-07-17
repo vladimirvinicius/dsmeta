1cbe803cacabaceaa4f2fc8126167b25ad151bca
Vladimir Vinicius <vladimirvinicius@gmail.com>
2022-07-17 16:02:53 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
@@ -12,7 +13,7 @@ function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then(response =>
     */
   axios(`${BASE_URL}/sales`).then((response) => {
-    console.log("SUCESSO");
+    toast.info("SMS enviado com sucesso")
   });
 }
 
